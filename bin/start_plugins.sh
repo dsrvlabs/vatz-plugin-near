aea50032694f3616d81d34f6ca0fab6060b6f581
xellos00 <xellos.kang@gmail.com>
2022-06-05 11:41:31 -0500

    Update make file

--- bin/start_plugins.sh
+++ bin/start_plugins.sh
@@ -2,7 +2,7 @@
 
 cd ..
 cd protocol-status-plugin
-go run main.go >> ../logs/protocol-status.log 2>&1 &
+make run
 cd ..
 cd machine-status-plugin
-go run main.go >> ../logs/server-status.log 2>&1 &
+make run
